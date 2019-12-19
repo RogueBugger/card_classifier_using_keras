accbccdd778201f28d747bcfcaedafd22f9e7a08
P. Muthukumar <roguebugger@gmail.com>
2019-12-20 01:45:02 +0530

    output the class name instead of array of value

--- predict_class.py
+++ predict_class.py
@@ -5,46 +5,33 @@
 from google.colab import drive
 #drive.mount('/content/gdrive')
 import cv2 as cv
+import keras
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
-#https://stackoverflow.com/questions/50227925/how-to-predict-from-saved-model-in-keras
+import json
 
 model = load_model('/content/gdrive/My Drive/dataset/model_multi_class_final.h5')
 
 model.compile(loss='categorical_crossentropy', 
 			optimizer='rmsprop', 
 			metrics=['accuracy']) 
-'''
-test_image = image.load_img('/content/gdrive/My Drive/dataset/test.jpg', target_size = (163, 260)) 
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-'''
 
+f = open('/content/gdrive/My Drive/dataset/class_indices.json')
+data = json.load(f)
 
-#img = cv.imread('/content/gdrive/My Drive/dataset/test.jpg')
-#result = model.predict(test_image)
-#result = model.predict(img)
-img = cv.imread('/content/gdrive/My Drive/dataset/test.jpg',0)
-img=cv.resize(img,dsize=(163, 260), interpolation=cv.INTER_CUBIC)
-img=np.expand_dims(img,0)
-img=np.expand_dims(img,axis=3)
+data = dict(map(reversed, data.items()))
 
-result = model.predict(img)
-print(result)
+def predict_class(path):
+	img = cv.imread(path,0)
+	img=cv.resize(img,dsize=(163, 260), interpolation=cv.INTER_CUBIC)
+	img=np.expand_dims(img,0)
+	img=np.expand_dims(img,axis=3)
+	result = model.predict(img)
+	print(result)
+	arr = result.argmax(axis=-1)
+	print(data[arr[0]])
 
-img = cv.imread('/content/gdrive/My Drive/dataset/tes1t.jpg',0)
-img=cv.resize(img,dsize=(163, 260), interpolation=cv.INTER_CUBIC)
-img=np.expand_dims(img,0)
-img=np.expand_dims(img,axis=3)
-
-result = model.predict(img)
-print(result)
-
-img = cv.imread('/content/gdrive/My Drive/dataset/tes2t.jpg',0)
-img=cv.resize(img,dsize=(163, 260), interpolation=cv.INTER_CUBIC)
-img=np.expand_dims(img,0)
-img=np.expand_dims(img,axis=3)
-
-result = model.predict(img)
-print(result)
+predict_class('/content/gdrive/My Drive/dataset/test.jpg')
+predict_class('/content/gdrive/My Drive/dataset/tes1t.jpg')
+predict_class('/content/gdrive/My Drive/dataset/tes2t.jpg')
